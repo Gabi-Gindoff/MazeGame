cae69668b3a56f408e2537e83486bcead31f88b8
Gabi-Gindoff <144366612+Gabi-Gindoff@users.noreply.github.com>
2023-11-30 12:40:53 -0500

    Update homePage.py

--- homePage.py
+++ homePage.py
@@ -1,8 +1,9 @@
-import numpy as np
-import pygame
-import sys
-import random
-import subprocess
+import numpy as np  # Import NumPy for numerical operations
+import pygame  # Import Pygame for game development
+import sys  # Import sys for system-specific parameters and functions
+import random  # Import random for random number generation
+import subprocess  # Import subprocess for spawning additional processes
+
  
 # Set up Pygame
 pygame.init()
@@ -10,42 +11,43 @@ pygame.init()
 # Set up the screen
 screen_width = 400
 screen_height = 300
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Maze Game")
- 
+screen = pygame.display.set_mode((screen_width, screen_height))  # Initialize game window
+pygame.display.set_caption("Maze Game")  # Set window caption
+
 # Set up fonts
-font = pygame.font.Font(None, 36)
+font = pygame.font.Font(None, 36)  # Define font for text rendering
+
  
 # Set up buttons
 button_width = 175
 button_height = 50
 button_spacing = 5  
  
-button_color = (0, 100, 255)
-button_hover_color = (0, 200, 0)
- 
+button_color = (0, 100, 255)  # Set color for buttons
+button_hover_color = (0, 200, 0)  # Set color for buttons when hovered
+
+# Define button rectangles - Play Button
 play_button = pygame.Rect(
     screen_width // 2 - button_width // 2,
     screen_height // 3,
     button_width,
-    button_height,
-    border_radius=10  # Set border radius for rounded corners
+    button_height
 )
- 
+
+# Define button rectangles - Quit Button
 quit_button = pygame.Rect(
     screen_width // 2 - button_width // 2,
     screen_height // 3 + button_height + button_spacing,
     button_width,
-    button_height,
-    border_radius=10  # Set border radius for rounded corners
+    button_height
 )
 
+# Define button rectangles - Instructions Button
 instructions_button = pygame.Rect(
     screen_width // 2 - button_width // 2,
     screen_height // 3 + 2 * (button_height + button_spacing),
     button_width,
-    button_height,
-    border_radius=10  # Set border radius for rounded corners
+    button_height
 )
  
 # Define game states
@@ -57,7 +59,7 @@ current_game_state = HOME_PAGE
  
  
 # Add a background image
-background_image = pygame.image.load("mazePic.jpeg")  # Replace "background.jpg" with your image file
+background_image = pygame.image.load("mazePic.jpeg")  
 background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
  
  
@@ -65,22 +67,24 @@ while True:  # Outer loop for restarting the game
  
     while True:  # Inner loop for each game session
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+            if event.type == pygame.QUIT: # To exit the application
+                pygame.quit() # Quit pygame
+                sys.exit() # Exit program
  
-            elif event.type == pygame.MOUSEBUTTONDOWN:
+            # Mouse Click Events - checks to see if buttons were clicked
+            elif event.type == pygame.MOUSEBUTTONDOWN: 
                 if current_game_state == HOME_PAGE:
                     # Check if multiplayer button is clicked on the home page
                     if play_button.collidepoint(event.pos):
                         print("Play button clicked")
-                        # Use subprocess to run twoplayer.py
+                        # Use subprocess to run maze.py file
                         subprocess.run(["python", "maze.py"])
 
  
                     # Check if quit button is clicked on the home page
                     elif quit_button.collidepoint(event.pos):
                         if quit_button.collidepoint(event.pos):
+                            # Quit the game if it is pressed
                             print("Quit button clicked")
                             pygame.quit()
                             sys.exit()
@@ -89,12 +93,13 @@ while True:  # Outer loop for restarting the game
                     # Check if instructions button is clicked on the home page
                     elif instructions_button.collidepoint(event.pos):
                         print("Instructions button clicked")
-                        # Display instructions page or perform any other action
+                        # Show the instructions page
                         subprocess.run(["python", "instructions.py"])
 
 
- 
+        # If the user is currently looking at the home page do the following
         if current_game_state == HOME_PAGE:
+            # Show background image
             screen.blit(background_image, (0, 0))
             # Draw welcome message
             welcome_text = font.render("Welcome to the Maze Game!", True, (0, 0, 0))
@@ -118,7 +123,8 @@ while True:  # Outer loop for restarting the game
             instructions_text = font.render("Instructions", True, (255, 255, 255))  # White text
             text_rect = instructions_text.get_rect(center=instructions_button.center)
             screen.blit(instructions_text, text_rect.topleft)
- 
+
+        # Update the display
         pygame.display.flip()
         pygame.time.Clock().tick(30)
  
